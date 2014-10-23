9cc67d093e2aed4b411e5bc22a8a8b0ae17399a9
karinasurgut <karinasurgut@gmail.com>
2014-10-23 17:52:49 +1100

    Check facebook login fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ After5::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => 'after5.herokuapp.com' }
 end
